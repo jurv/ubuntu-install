05e58715941572e2036abe23e619e4e666d03391
Julien Rouvier <jul.rouvier@gmail.com>
2018-08-15 16:03:13 +0200

    Add atom packages

--- modules/atom/install.sh
+++ modules/atom/install.sh
@@ -8,5 +8,16 @@ then
 	# the installation will be completed by the following apt-get install
 	sudo dpkg -i atomio.deb >$LOG_STEPS 2>&1
 	try sudo apt-get install -f -y
+
+	# Install useful packages
+	try python -m pip install 'python-language-server[all]'
+	try apm install atom-ide-ui
+	try apm install ide-python
+	try apm install atom-django
+	try apm install django-templates
+
+	# After install, the path to the python executable must be changed
+	# in conf from 'python' to 'python3'
+
 fi
 next
