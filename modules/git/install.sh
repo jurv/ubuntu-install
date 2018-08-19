e251d3a91ca12c1b188287acc483985f31b597fc
Julien Rouvier <jul.rouvier@gmail.com>
2018-08-19 18:42:03 +0200

    Install git prompt
    https://github.com/magicmonty/bash-git-prompt

--- modules/git/install.sh
+++ modules/git/install.sh
@@ -1,3 +1,19 @@
 step "Installing Git..."
+
 try sudo apt-get install -y git
+
+# Git prompt
+if [ ! -d $HOMEDIR/.bash-git-prompt ]
+then
+
+    try cd $HOMEDIR
+
+    try git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt --depth=1
+
+    try sudo sh -c "cp $HOMEDIR/.bashrc $HOMEDIR/.bashrc.bck"
+    try sudo sh -c "cat $INSTALL_DIR/modules/git/bashrc-config >> $HOMEDIR/.bashrc"
+
+    try cd -
+fi
+
 next
