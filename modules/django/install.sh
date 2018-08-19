1713b83fabf950e346bb3290396db0f654dd7ba9
Julien Rouvier <jul.rouvier@gmail.com>
2018-08-19 15:02:13 +0200

    Install Django

---
+++ modules/django/install.sh
@@ -0,0 +1,8 @@
+step "Installing Django... "
+
+try pip3 install Django==2.0
+
+# Library to handle images
+try pip install pillow
+
+next
--- modules/python3/install.sh
+++ modules/python3/install.sh
@@ -1,7 +1,3 @@
 step "Installing Python3..."
 try sudo apt-get install -y python3 python3-pip python3-dev build-essential
-
-# While we're at it, install Django
-try pip3 install Django==2.0
-
 next
